97be9097fcf917c00f1a98e994ef19671f6918b4
Mario91210 <58263108+Mario91210@users.noreply.github.com>
2020-06-07 15:59:20 -0500

    Add files via upload

---
+++ Py/especial.py
@@ -0,0 +1,29 @@
+import openpyxl
+import PIL
+import pandas
+import matplotlib
+
+workbook = "proyectoespecial.xlsx"
+df = pd.read_excel(workbook)
+
+print(df.head())
+
+valores = df[["Pendiente","Porcentaje"]]
+print(valores)
+
+ax = valores.plot.bar(x = "Pendiente", y = "Porcentaje", rot = 0)
+plt.show()
+
+#Si desea ver la imagen del área de estudio indique "y"
+run = raw_input("Desea ver el Ejido de estudio y/n\n")
+
+
+from PIL import Image
+
+
+#Para que la imagen pueda ser expuesta deberá de indicarse con el nombre de la imagen
+def show(path_name):
+    image = ()
+image.open(path_name)=
+    image.show()
+show()
